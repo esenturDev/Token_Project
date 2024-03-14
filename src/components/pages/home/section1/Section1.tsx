dbcfe0d5da87af5cd27c9e3b8070ca81a8d91c86
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-15 00:22:23 +0600

    update

--- src/components/pages/home/section1/Section1.tsx
+++ src/components/pages/home/section1/Section1.tsx
@@ -1,24 +1,47 @@
-import { useGetProductsQuery } from '../../../../redux/api/productApi';
-import scss from './Section1.module.scss';
+import { useGetProductsQuery } from "../../../../redux/api/productApi";
+import scss from "./Section1.module.scss";
+import logo from "../../../../assets/photos/streamline_interface-favorite-heart-reward-social-rating-media-heart-it-like-favorite-love.png";
+import { useState } from "react";
 
 const Section1 = () => {
-  const {data: productsArray = []} =  useGetProductsQuery();
+	const { data: productsArray = [] } = useGetProductsQuery();
+	const [likeState, setLikeState] = useState<number | boolean>(false);
 	console.log(productsArray);
-  return (
-    <div className={scss.section1}>
-      <div className="container">
-        <div className={scss.content}>
-          {productsArray.map((item) => (
-            <div key={item._id}>
-              <img src={item.photoUrl} alt={item.productName} />
-              <p>{item.productName}</p>
-              <p>{item.price}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
+	const handleLike = (id: number) => {
+		setLikeState(id);
+	};
+	return (
+		<div className={scss.section1}>
+			<div className="container">
+				<div className={scss.content}>
+					{productsArray.map((item) => (
+						<div className={scss.produc} key={item._id}>
+							<img src={item.photoUrl} alt={item.productName} />
+							<div className={scss.mapContents}>
+								<div className={scss.div1}>
+									<p className={scss.p}>{item.productName}</p>
+									<p>{item.price}</p>
+								</div>
+								<div className={scss.inonsLikeDiv}>
+									<img
+										className={
+											likeState === item._id
+												? `${scss.likeActive}`
+												: `${scss.activeNoo}`
+										}
+										onClick={() => handleLike(item._id!)}
+										src={logo}
+										alt="logo like"
+									/>
+								</div>
+							</div>
+							<button>Добавить в корзину</button>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Section1
+export default Section1;
--- src/redux/api/productApi.ts
+++ src/redux/api/productApi.ts
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Products } from "./types";
 
-
 // interface CreateUserRequest {
 //   id?: number;
 //   email: string;
@@ -12,22 +11,31 @@ import { Products } from "./types";
 export const productApi = createApi({
 	reducerPath: "productsApi",
 	baseQuery: fetchBaseQuery({ baseUrl: "https://brand-store.up.railway.app" }),
+	tagTypes: ["productsGet"],
 	endpoints: (builder) => {
 		return {
-			getProducts: builder.query<Products.GetProductsResponse, Products.GetProductsRequest>({
+			getProducts: builder.query<
+				Products.GetProductsResponse,
+				Products.GetProductsRequest
+			>({
 				query: () => ({
 					url: "products",
 					headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 				}),
+				providesTags: ["productsGet"],
 			}),
-			postProducts: builder.mutation<Products.PostProductsResponse, Products.PostProductsRequest>({
-				query: ({productName, price, quantity, photoUrl}) => ({
+			postProducts: builder.mutation<
+				Products.PostProductsResponse,
+				Products.PostProductsRequest
+			>({
+				query: ({ productName, price, quantity, photoUrl }) => ({
 					url: "products",
 					method: "POST",
-					body: {productName, price, quantity, photoUrl},
+					body: { productName, price, quantity, photoUrl },
 					headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
-				})
-			})
+				}),
+				invalidatesTags: ["productsGet"],
+			}),
 			// createUser: builder.mutation<CreateUserResponse, CreateUserRequest>({
 			//   query: ({email, password, userName}) => ({
 			//     url: 'users',
